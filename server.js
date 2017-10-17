6c5e32f975ec6750eeb7d47ea32fca93d91fbb3f
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-16 21:04:42 -0400

    Have cheerio and request working together.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
+var mongoose = require('mongoose')
 
-var bodyParser = require('body-parser')
-
-var cheerio = require('cheerio')
 
 var app = express();
 
 var PORT = process.env.PORT || 2408
 
+
+var bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,7 +20,33 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 
 app.set('view engine', 'handlebars')
 
+var request = require('request')
+var cheerio = require('cheerio')
 
+request("https://www.nytimes.com/", function(error, response, html){
+
+var $ = cheerio.load(html);
+
+var results = [];
+
+
+$("h2.story-heading").each(function(i, element){
+    
+
+    var title = $(element).text();
+
+    var link = $(element).children().attr("href")
+
+
+    results.push({
+        title: title,
+        link: link
+    })
+})
+
+
+console.log(results);
+})
 
 app.listen(PORT , function(){
     console.log("App running on Port " + PORT);
